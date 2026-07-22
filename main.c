ce27940f938563062d187f68c57111d193267cfa
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-22 19:03:26 -0300

    adicionando as posições

--- main.c
+++ main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 
 int main() {
+    char posicao[9];
+    
+    for(int i=0; i<9; i++){
+    	posicao[i] = 49 + i;
+	}
     
     printf("       |       |       \n");
-    printf("       |       |       \n");
+    printf("   %c   |   %c   |    %c  \n", posicao[0], posicao[1], posicao[2]);
     printf("_______|_______|_______\n");
     printf("       |       |       \n");
-    printf("       |       |       \n");
+    printf("   %c   |   %c   |    %c  \n", posicao[3], posicao[4], posicao[5]);
     printf("_______|_______|_______\n");
     printf("       |       |       \n");
-    printf("       |       |       \n");
+    printf("   %c   |   %c   |    %c  \n", posicao[6], posicao[7], posicao[8]);
     printf("       |       |       \n");
 
     return 0;
