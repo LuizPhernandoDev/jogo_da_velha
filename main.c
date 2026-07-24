b16873924a334270b6c510de1fac3570f07c2bf2
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-24 13:35:16 -0300

    alterando condição de vitoria

--- main.c
+++ main.c
@@ -5,12 +5,12 @@
 
 void ImprimeLayout(char velha[],int n);
 void ImprimeInicial();
-char CondicaoVitoria(char velha[], int n, char vez);
+int CondicaoVitoria(char velha[], int n);
 
 int main() {
 	setlocale(LC_ALL, "Portuguese");
-    char velha[9], vez, posicao, vencedor = 'a';
-    int disponivel, jogadas;
+    char velha[9], vez, posicao;
+    int disponivel, jogadas, vitoria;
     
     for(int i=0; i<9; i++)
     	velha[i] = 49 + i;
@@ -59,7 +59,9 @@ int main() {
 			
 		}while(posicao < 49 || posicao > 57 || !(disponivel));
 		
-		vencedor = CondicaoVitoria(velha, sizeof(velha), vez);
+		vitoria = CondicaoVitoria(velha, sizeof(velha));
+		if(vitoria)
+			break;
 		
 		if(vez == 'X')
 			vez = 'O';
@@ -68,11 +70,11 @@ int main() {
 			
 		jogadas++;
 		
-	}while(vencedor == 'a' && jogadas <= 9);
+	}while(jogadas <= 9);
 	
-	if(vencedor != 'a'){
+	if(vez != 'a'){
 		printf("Parabens!\n");
-		printf("O %c é o vencedor.\n", vencedor);
+		printf("O %c é o vencedor.\n", vez);
 	}else
 	printf("Deu empate!\n");
 
@@ -125,12 +127,24 @@ void ImprimeLayout(char velha[], int n){
     printf("       |       |       \n");
 }
 
-char CondicaoVitoria(char velha[], int n, char vez){
-	if(velha[0] == velha[1] && velha[0] == velha[2] || velha[3] == velha[4] && velha[3] == velha[5] || velha[6] == velha[7] && velha[6] == velha[8])
-		return vez;
-	if(velha[0] == velha[3] && velha[0] == velha[6] || velha[1] == velha[4] && velha[1] == velha[7] || velha[2] == velha[5] && velha[2] == velha[8])
-		return vez;
-	if(velha[0] == velha[4] && velha[0] == velha[8] || velha[2] == velha[4] && velha[2] == velha[6])
-		return vez;
-	return 'a';
+int CondicaoVitoria(char velha[], int n){
+	if(velha[0] == velha[1] && velha[0] == velha[2])
+		return 1;
+	else if(velha[3] == velha[4] && velha[3] == velha[5])
+		return 2;
+	else if(velha[6] == velha[7] && velha[6] == velha[8])
+		return 3;
+	else if(velha[0] == velha[3] && velha[0] == velha[6])
+		return 4;
+	else if(velha[1] == velha[4] && velha[1] == velha[7])
+		return 5;
+	else if(velha[2] == velha[5] && velha[2] == velha[8])
+		return 6;
+	else if(velha[0] == velha[4] && velha[0] == velha[8])
+		return 7;
+	else if(velha[2] == velha[4] && velha[2] == velha[6])
+		return 8;
+	else
+		return 0;
+   
 }
