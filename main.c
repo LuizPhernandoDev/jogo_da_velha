88ca8b27d497a392b2b47f6178146f342a84e8b7
Luiz Phernando Neves Alves de Souza <luizphernando2905@gmail.com>
2026-07-23 21:46:16 -0300

    adicionando troca de jogadores

--- main.c
+++ main.c
@@ -17,31 +17,45 @@ int main() {
 		ImprimeInicial();
 		printf("Precione [X] ou [O] para começar: ");
 		vez = getchar();
+		
 		system("cls");
+		
 		if(vez != 'x' && vez != 'o' && vez != 'X' && vez != 'O')
-			printf("                    Invalido! \n");
+			printf("          Invalido!\n");
 		else if(vez == 'x' || vez == 'o')
 			vez-=32;
+			
 	}while(vez!='X' && vez!='O');
-    
-	do{
-		ImprimeLayout(velha, sizeof(velha));
-		printf("É a vez do %c jogar, escolha a posição: \n", vez);
-		printf("Disponives: ");
-		for(i=0; i<9; i++)
-			if(velha[i] == 49+i)
-				printf("%c ", velha[i]);
+    do{
+    	do{
+			ImprimeLayout(velha, sizeof(velha));
+			
+			printf("É a vez do %c jogar, escolha a posição: \n", vez);
+			printf("Disponives: ");
+			
+			for(i=0; i<9; i++)
+				if(velha[i] == 49+i)
+					printf("[%c] ", velha[i]);	
 			putchar('\n');
+			
 			fflush(stdin);
 			posicao = getchar();
+			
 			system("cls");
-			if(posicao < 49 || posicao >= 57)
-				printf("      Invalido! \n");
+			
+			if(posicao < 49 || posicao > 57)
+				printf("          Invalido!\n");
 			else
 				velha[posicao-49] = vez;
-	}while(posicao < 49 || posicao >= 57);
-    
-    ImprimeLayout(velha, sizeof(velha));
+				
+		}while(posicao < 49 || posicao > 57);
+		
+		if(vez == 'X')
+			vez = 'O';
+		else
+			vez = 'X';
+			
+	}while(1);
 
     return 0;
 }
